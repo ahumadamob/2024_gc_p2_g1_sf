e5fde35631103dd7e6ccb428278b484a14e71fb0
ChuchuGiampo <chulogiamportone@gmail.com>
2024-09-06 15:10:34 -0300

    persistencia simple Factura

---
+++ src/main/java/gc/_4/pr2/grupo1/repository/FacturaRepository.java
@@ -0,0 +1,10 @@
+package gc._4.pr2.grupo1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import gc._4.pr2.grupo1.entity.Factura;
+
+
+public interface FacturaRepository extends JpaRepository<Factura, Long> {
+
+}
