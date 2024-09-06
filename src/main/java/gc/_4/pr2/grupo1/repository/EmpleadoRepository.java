6efb7bb5ea3db53e169d9b801c94a8bbe377c734
FacundoGiannone <fngiannone23@gmail.com>
2024-09-06 18:39:35 -0300

    persistencia simple empleado

---
+++ src/main/java/gc/_4/pr2/grupo1/repository/EmpleadoRepository.java
@@ -0,0 +1,9 @@
+package gc._4.pr2.grupo1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import gc._4.pr2.grupo1.entity.Empleado;
+
+public interface EmpleadoRepository extends JpaRepository<Empleado, Long> {
+
+}
