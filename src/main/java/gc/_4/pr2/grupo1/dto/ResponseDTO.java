3130a40e6e038c9bee2f09d8d3948f4a5c8ab2b2
AraMirta <arabelmirta14@gmail.com>
2024-10-30 00:25:42 -0300

    realizada la Persistencia avanzada en el DTO.

---
+++ src/main/java/gc/_4/pr2/grupo1/dto/ResponseDTO.java
@@ -0,0 +1,47 @@
+package gc._4.pr2.grupo1.dto;
+
+public class ResponseDTO <T> {
+	
+	private boolean status;
+	private String message;
+	private T data;
+	
+	public ResponseDTO(boolean status, String message, T data) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.data = data;
+	}
+	
+	public ResponseDTO() {
+		
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	
+
+
+}
